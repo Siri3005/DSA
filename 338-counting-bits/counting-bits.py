f5d7db8685227993b0e3658aca920652a8d79297
Siri3005 <144516796+Siri3005@users.noreply.github.com>
2025-07-07 17:04:30 +0530

    Time: 7 ms (79.51%) | Memory: 17.9 MB (46.00%) - LeetSync

--- 338-counting-bits/counting-bits.py
+++ 338-counting-bits/counting-bits.py
@@ -1,14 +1,9 @@
 class Solution(object):
     def countBits(self, n):
-        result=[0]*(n+1)
-        for i in range(n+1):
-            result[i]=self.count_ones(i)
-        return result
-        
-    def count_ones(self,n):
-        res=0
-        while n:
-            res+=n%2
-            n=n>>1
-        return res
-        
+        dp=[0]*(n+1)
+        offset=1
+        for i in range(1,n+1):
+            if offset*2==i:
+                offset=i
+            dp[i]=1+dp[i-offset]
+        return dp

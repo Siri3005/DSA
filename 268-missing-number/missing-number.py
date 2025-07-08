c6c14559ed4b3676d7f3f79c87b725114cc87283
Siri3005 <144516796+Siri3005@users.noreply.github.com>
2025-07-08 16:32:14 +0530

    Time: 2655 ms (9.41%) | Memory: 13.3 MB (61.99%) - LeetSync

---
+++ 268-missing-number/missing-number.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def missingNumber(self, nums):
+        for i in range(len(nums)):
+            if i not in nums:
+                return i
+        return len(nums)
+        

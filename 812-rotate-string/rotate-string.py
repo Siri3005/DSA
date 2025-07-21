6121e20310deefe47fa59b647ab402934f232592
Siri3005 <144516796+Siri3005@users.noreply.github.com>
2025-07-21 18:51:17 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (80.01%) - LeetSync

---
+++ 812-rotate-string/rotate-string.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def rotateString(self, s, goal):
+        if s==goal:
+            return True
+        arr=list(s)
+        goal=list(goal)
+        for i in range(len(s)-1):
+            ele=arr.pop()
+            arr.insert(0,ele)
+            if arr==goal:
+                return True
+        return False

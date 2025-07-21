ff995ac903b414af08a9cd72fb96d78c55b15892
Siri3005 <144516796+Siri3005@users.noreply.github.com>
2025-07-21 20:21:43 +0530

    Time: 3 ms (78.02%) | Memory: 12.5 MB (22.98%) - LeetSync

---
+++ 837-most-common-word/most-common-word.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def mostCommonWord(self, paragraph, banned):
+        hashmap={}
+        
+        for ch in string.punctuation:
+            paragraph=paragraph.replace(ch,' ')
+        arr=paragraph.lower().split()
+        for word in arr:
+            if word not in banned:
+                hashmap[word]=hashmap.get(word,0)+1
+        maxm=0
+        res=''
+        for key,val in hashmap.items():
+            if val>maxm:
+                maxm=val
+                res=key
+        return res

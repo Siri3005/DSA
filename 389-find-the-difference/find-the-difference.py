7b67b12651173e2f459a8b63d094b11fe84c7724
Siri3005 <144516796+Siri3005@users.noreply.github.com>
2025-07-21 18:57:13 +0530

    Time: 26 ms (6.14%) | Memory: 12.4 MB (91.03%) - LeetSync

---
+++ 389-find-the-difference/find-the-difference.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def findTheDifference(self, s, t):
+        s=list(s)
+        for i in range(len(t)):
+            if t[i] not in s:
+                return t[i]
+            s.remove(t[i])
+        

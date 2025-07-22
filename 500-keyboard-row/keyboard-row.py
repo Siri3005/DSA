2084dcfd09e6a41458bf8ae3b0316ce66bdcc448
Siri3005 <144516796+Siri3005@users.noreply.github.com>
2025-07-22 07:16:48 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (51.76%) - LeetSync

--- 500-keyboard-row/keyboard-row.py
+++ 500-keyboard-row/keyboard-row.py
@@ -1,11 +1,20 @@
 class Solution(object):
     def findWords(self, words):
-        first=set('qwertyuiop')
+        '''first=set('qwertyuiop')
         second=set('asdfghjkl')
         third=set('zxcvbnm')
         res=[]
         for word in words:
             lower_word_set = set(word.lower()) 
+            if lower_word_set.issubset(first) or lower_word_set.issubset(second) or lower_word_set.issubset(third):
+                res.append(word)
+        return res'''
+        first=set('qwertyuiop')
+        second=set('asdfghjkl')
+        third=set('zxcvbnm')
+        res=[]
+        for word in words:
+            lower_word_set=set(word.lower())
             if lower_word_set.issubset(first) or lower_word_set.issubset(second) or lower_word_set.issubset(third):
                 res.append(word)
         return res

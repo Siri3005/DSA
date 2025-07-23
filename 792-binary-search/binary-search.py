dd0ce0c1ff8312400f883e2193559fa42fa6bfec
Siri3005 <144516796+Siri3005@users.noreply.github.com>
2025-07-23 20:03:22 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (32.36%) - LeetSync

---
+++ 792-binary-search/binary-search.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def search(self, nums, target):
+        left=0
+        right=len(nums)-1
+        if len(nums)==1 and nums[0]==target:
+            return 0
+        while left<=right:
+            mid=(left+right)//2
+            if nums[mid]==target:
+                return mid
+            elif nums[mid]>target:
+                right=mid-1
+            elif nums[mid]<target:
+                left=mid+1
+        return -1
+            
+            

f9f94e4baf893312238571c99a9a1bdf45575d3d
Siri3005 <144516796+Siri3005@users.noreply.github.com>
2025-07-25 14:10:41 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (6.77%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.py
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.py
@@ -17,6 +17,10 @@ class Solution(object):
             tail=tail.next
         if list1:
             tail.next=list1
-        elif list2:
+        else:
             tail.next=list2
         return dummy.next
+
+
+
+            

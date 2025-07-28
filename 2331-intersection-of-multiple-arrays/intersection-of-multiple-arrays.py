87ad3f4d2997ec326d741a922ec5b1e917631e12
Siri3005 <144516796+Siri3005@users.noreply.github.com>
2025-07-28 11:52:48 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (79.49%) - LeetSync

--- 2331-intersection-of-multiple-arrays/intersection-of-multiple-arrays.py
+++ 2331-intersection-of-multiple-arrays/intersection-of-multiple-arrays.py
@@ -1,11 +1,7 @@
 class Solution(object):
     def intersection(self, nums):
-        if len(nums)==1:
-            return sorted(nums[0])
-        pre=set(nums[0])
-        res=[]
+        res=set(nums[0])
         for i in range(1,len(nums)):
             curr=set(nums[i])
-            res=list(pre & curr)
-            pre=set(res)
-        return sorted(res)
+            res=res & curr
+        return sorted(list(res))

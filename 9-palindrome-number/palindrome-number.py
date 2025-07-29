e7f6dc9e7788ad147728d6673eb87ff9443dcffc
Siri3005 <144516796+Siri3005@users.noreply.github.com>
2025-07-29 12:32:43 +0530

    Time: 8 ms (69.80%) | Memory: 12.5 MB (51.47%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        str_x=str(x)
+        left=0
+        right=len(str_x)-1
+        while left<right:
+            if str_x[left]!=str_x[right]:
+                return False
+            left+=1
+            right-=1
+        return True
+                

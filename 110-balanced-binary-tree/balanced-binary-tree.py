23a80842e674949d068c62fc774804a03c501c31
Siri3005 <144516796+Siri3005@users.noreply.github.com>
2025-08-09 10:28:19 +0530

    Time: 13 ms (19.86%) | Memory: 18.2 MB (11.61%) - LeetSync

---
+++ 110-balanced-binary-tree/balanced-binary-tree.py
@@ -0,0 +1,18 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def isBalanced(self, root):
+        def helper(root):
+            if not root:
+                return [True,0]
+            left=helper(root.left)
+            right=helper(root.right)
+            balance=left[0] and right[0] and abs(left[1]-right[1])<=1
+            return [balance,1+max(left[1],right[1])]
+        res=helper(root)
+        return res[0]
+        

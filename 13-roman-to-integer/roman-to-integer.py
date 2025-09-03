a125231dd685c9ee7651a73b23b2caf45a7e830b
Siri3005 <144516796+Siri3005@users.noreply.github.com>
2025-09-03 16:15:14 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (98.75%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.py
+++ 13-roman-to-integer/roman-to-integer.py
@@ -1,13 +1,14 @@
-class Solution(object):
-    def romanToInt(self, s):
-        hashmap={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        dicti={'I':1,'V':5,'X':10,'L':50,'C':100,'D':500,'M':1000}
         total=0
         prev=0
         for ch in reversed(s):
-            curr=hashmap[ch]
+            curr=dicti[ch]
             if curr<prev:
                 total-=curr
             else:
                 total+=curr
                 prev=curr
         return total
+        

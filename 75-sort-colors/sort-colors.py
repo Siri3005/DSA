8127bafb52623c27283ffd13d037fa1e74ce8d65
Siri3005 <144516796+Siri3005@users.noreply.github.com>
2025-09-04 10:40:55 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (11.88%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -1,3 +1,16 @@
-class Solution(object):
-    def sortColors(self, nums):
-        return nums.sort()
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        left=0
+        right=len(nums)-1
+        mid=0
+        while mid<=right:
+            if nums[mid]==0:
+                nums[left],nums[mid]=nums[mid],nums[left]
+                left+=1
+                mid+=1
+            elif nums[mid]==1:
+                mid+=1
+            else:
+                nums[mid],nums[right]=nums[right],nums[mid]
+                right-=1
+        

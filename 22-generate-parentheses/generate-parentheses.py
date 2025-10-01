d6eff27981cb83c354293b5fe278b4643f12d8b3
Siri3005 <144516796+Siri3005@users.noreply.github.com>
2025-10-01 15:04:49 +0530

    Time: 1 ms (65.61%) | Memory: 12.7 MB (75.97%) - LeetSync

---
+++ 22-generate-parentheses/generate-parentheses.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def generateParenthesis(self, n):
+        res=[]
+        def backtrack(s,ocount,ccount):
+            if len(s)==2*n:
+                res.append(s)
+                return
+            if ocount<n:
+                backtrack(s+'(',ocount+1,ccount)
+            if ccount<ocount:
+                backtrack(s+')',ocount,ccount+1)
+        backtrack('',0,0)
+        return res
+        

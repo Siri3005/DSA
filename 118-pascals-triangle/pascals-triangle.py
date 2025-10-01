0db4ffd61e65799dcc31cda0e126bdbcccfc30d9
Siri3005 <144516796+Siri3005@users.noreply.github.com>
2025-10-01 11:47:09 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (56.53%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -1,18 +1,20 @@
 class Solution(object):
     def generate(self, numRows):
-        arr=[1]
+        first_row=[1]
         res=[]
-        res.append(arr)
-        prev_arr=arr
+        res.append(first_row)
+        pre_row=first_row
         for i in range(1,numRows):
-            new_arr=[]
+            curr_row=[1]*(i+1)
             for j in range(i+1):
                 if j==0 or j==i:
-                    new_arr.append(1)
+                    continue
                 else:
-                    new_arr.append(prev_arr[j-1]+prev_arr[j])
-            res.append(new_arr)
-            prev_arr=new_arr
+                    curr_row[j]=pre_row[j]+pre_row[j-1]
+            res.append(curr_row)
+            pre_row=curr_row
         return res
+
+
 
         

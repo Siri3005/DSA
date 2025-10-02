7865ff3530055557f130cf067d6ea53c7c6f5ccb
Siri3005 <144516796+Siri3005@users.noreply.github.com>
2025-10-02 09:07:05 +0530

    Time: 7 ms (95.18%) | Memory: 12.5 MB (80.26%) - LeetSync

--- 39-combination-sum/combination-sum.py
+++ 39-combination-sum/combination-sum.py
@@ -2,11 +2,8 @@ class Solution(object):
     def combinationSum(self, candidates, target):
         dp=[[] for _ in range(target+1)]
         dp[0]=[[]]
-        dp[1]=[]
         for cand in candidates:
             for t in range(cand,target+1):
                 for comb in dp[t-cand]:
                     dp[t].append(comb+[cand])
         return dp[target]
-
-        

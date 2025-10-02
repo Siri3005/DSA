41cc3ee33a837c033883349ea96032a246115366
Siri3005 <144516796+Siri3005@users.noreply.github.com>
2025-10-02 09:37:13 +0530

    Time: 3 ms (26.39%) | Memory: 12.4 MB (85.76%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,12 +1,14 @@
 class Solution(object):
     def uniquePaths(self, m, n):
-        dp=[[0]* n for _ in range(m)]
-        for i in range(n):
+        dp=[[0]*n for _ in range(m)]
+        dp[0][0]=1
+        for i in range(1,n):
             dp[0][i]=1
-        for j in range(m):
+        for j in range(1,m):
             dp[j][0]=1
         for r in range(1,m):
             for c in range(1,n):
                 dp[r][c]=dp[r-1][c]+dp[r][c-1]
-        return dp[m-1][n-1]
+        return dp[-1][-1]
+
         

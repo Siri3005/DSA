4efd0b552b1f5323e3d2da8c5d1586fa584b6e2e
Siri3005 <144516796+Siri3005@users.noreply.github.com>
2025-10-02 09:32:17 +0530

    Time: 0 ms (100.00%) | Memory: 12.6 MB (15.55%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.py
+++ 213-house-robber-ii/house-robber-ii.py
@@ -1,16 +1,16 @@
 class Solution(object):
     def rob(self, nums):
-        def cal_mon(nums):
+        def helper(nums):
             n=len(nums)
             dp=[0]*(n+1)
             dp[0]=0
             dp[1]=nums[0]
-            n=len(nums)
             for i in range(2,n+1):
                 dp[i]=max(dp[i-1],nums[i-1]+dp[i-2])
             return max(dp[-1],dp[-2])
         if len(nums)==1:
             return nums[0]
-        dp1=cal_mon(nums[:-1])
-        dp2=cal_mon(nums[1:])
+        dp1=helper(nums[:-1])
+        dp2=helper(nums[1:])
         return max(dp1,dp2)
+        

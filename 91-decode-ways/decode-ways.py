52adf843a7afab02eea636cab1240fb3b1e5af91
Siri3005 <144516796+Siri3005@users.noreply.github.com>
2025-10-02 08:45:23 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (89.46%) - LeetSync

--- 91-decode-ways/decode-ways.py
+++ 91-decode-ways/decode-ways.py
@@ -1,6 +1,6 @@
 class Solution(object):
     def numDecodings(self, s):
-        if not s or s[0]=='0':
+        if len(s)==0 or s[0]=='0':
             return 0
         n=len(s)
         dp=[0]*(n+1)
@@ -9,7 +9,7 @@ class Solution(object):
         for i in range(2,n+1):
             if s[i-1]!='0':
                 dp[i]+=dp[i-1]
-            two_digit=int(s[i-2:i])
-            if two_digit>=10 and two_digit<=26:
+            string=int(s[i-2:i])
+            if string>=10 and string<=26:
                 dp[i]+=dp[i-2]
         return dp[-1]

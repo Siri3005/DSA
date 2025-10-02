b09e222dde6f385a915d75cee44e69ec91384bfd
Siri3005 <144516796+Siri3005@users.noreply.github.com>
2025-10-02 09:44:54 +0530

    Time: 0 ms (100.00%) | Memory: 12.7 MB (76.13%) - LeetSync

--- 22-generate-parentheses/generate-parentheses.py
+++ 22-generate-parentheses/generate-parentheses.py
@@ -4,11 +4,12 @@ class Solution(object):
         def backtrack(s,ocount,ccount):
             if len(s)==2*n:
                 res.append(s)
-                return
+                return 
             if ocount<n:
                 backtrack(s+'(',ocount+1,ccount)
             if ccount<ocount:
                 backtrack(s+')',ocount,ccount+1)
-        backtrack('',0,0)
+        backtrack("",0,0)
         return res
+
         

325aa647a1ec4993abf7faa28d4c550f398bdc05
Siri3005 <144516796+Siri3005@users.noreply.github.com>
2025-10-02 09:28:31 +0530

    Time: 0 ms (100.00%) | Memory: 12.5 MB (15.11%) - LeetSync

--- 198-house-robber/house-robber.py
+++ 198-house-robber/house-robber.py
@@ -1,10 +1,10 @@
 class Solution(object):
     def rob(self, nums):
-        n=len(nums)+1
-        dp=[0]*n
+        n=len(nums)
+        dp=[0]*(n+1)
         dp[0]=0
         dp[1]=nums[0]
-        for i in range(2,n):
+        for i in range(2,n+1):
             dp[i]=max(dp[i-1],nums[i-1]+dp[i-2])
         return max(dp[-1],dp[-2])
         
